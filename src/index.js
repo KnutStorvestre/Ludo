daa8803e9c55178249e11620e0b45730c8fe1dee
Knut Storvestre <huq006@uib.no>
2020-01-30 14:24:37 +0100

    fixed bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Game from "./Game";
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
 
